2401efdeb6aee3a689d3b05783d2f770c56c10b9
Andrew Burks <andrewtburks@gmail.com>
2018-04-11 21:01:30 -0500

    Lay out page basics

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { PageHeader, Nav, NavItem, Well } from 'react-bootstrap';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { page: "home" };
+
+    this.onNavSelect = this.onNavSelect.bind(this);
+  }
+
+  onNavSelect(selectedKey) {
+    this.setState({ page: selectedKey });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <PageHeader style={{ padding: "0 50px"}}>
+          Andrew Burks <small>Ph.D Computer Science Student</small>
+        </PageHeader>
+        <div className="pageContent" style={{width: "80%", margin: "auto"}}>
+          <Nav bsStyle="pills" activeKey={this.state.page} onSelect={this.onNavSelect}>
+            <NavItem eventKey={"home"}>
+              Home
+            </NavItem>
+            <NavItem eventKey={"about"}>
+              About
+            </NavItem>
+            <NavItem eventKey={"proj"}>
+              Projects
+            </NavItem>
+            <NavItem eventKey={"pub"}>
+              Publications
+            </NavItem>
+            <NavItem eventKey={"contact"}>
+              Contact
+            </NavItem>
+          </Nav>
+          <Well style={{ margin: "10px 0" }}>
+          </Well>
+        </div>
+      </Fragment>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
---
+++ src/components/project.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+  state = {}
+  render() {
+    let { name, abstract, team, imgref, toolref, reporef, pdfref } = this.props;
+
+    return ( 
+      <div>
+        name
+      </div>
+     )
+  }
+}
+ 
+export default Project;
