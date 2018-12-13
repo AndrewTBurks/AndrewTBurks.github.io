c2d4d95031ba2c412cc2c185a39d9171c7ab5b80
Andrew Burks <andrewtburks@gmail.com>
2018-12-13 12:31:31 -0600

    try lazy loading, fail

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import { Router } from '@reach/router';
 
+// const Home = React.lazy(() => import("./pages/Home"));
+// const Projects = React.lazy(() => import("./pages/Projects"));
+// const Publications = React.lazy(() => import("./pages/Publications"));
+// const About = React.lazy(() => import("./pages/About"));
 
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Publications from "./pages/Publications";
-
 import About from './pages/About';
-
 
 
 import Typography from './components/Typography';
 
 import projectData from "./projects.json";
 import pubData from "./pubs.json";
-
-const TestPage = (props) => {
-  return (
-    <div style={{color: props.color}}>
-      TestPage
-    </div>
-  );
-};
-
-const FontTest = () => {
-  return <>
-      {/* <div className="panel"> */}
-        <Typography type="heading">Heading</Typography>
-        <Typography type="subheading">Subheading</Typography>
-        <Typography type="subsubheading">Subsubheading</Typography>
-        <Typography type="link">link</Typography>
-        <Typography type="default">default</Typography>
-        <Typography type="caption">caption</Typography>
-        <Typography type="subcaption">subcaption</Typography>
-      {/* </div> */}
-    </>;
-}
+import { Loader } from 'react-loader-spinner';
 
 export default function Content(props) {
   return <div className="contentWrapper">
-      <Router>
-        <Home default path="/" />
-        <About path="/about" />
-        <Projects path="/projects" projects={projectData} showModal={props.showModal}/>
-        <Publications path="publications" pubs={pubData}/>
-        {/* <About path="/about" />
-        <About path="/about" /> */}
-      </Router>
+      <React.Suspense fallback={
+        <div style={{display: "flex", alignItems:"center", justifyContent: "center", width: "100%"}}>
+          <Loader
+            type="Triangle"
+            color="var(--purple)"
+            height="100"
+            width="100"
+          />
+        </div>
+      }>
+        <Router>
+          <Home default path="/" />
+          <About path="/about" />
+          <Projects path="/projects" projects={projectData} showModal={props.showModal}/>
+          <Publications path="publications" pubs={pubData}/>
+        </Router>
+      </React.Suspense>
     </div>;
 }
