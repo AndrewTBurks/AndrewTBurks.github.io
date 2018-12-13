209d99edf89eec46a099c52c5b99fd54c62f23ba
Andrew Burks <andrewtburks@gmail.com>
2018-12-13 12:32:13 -0600

    Updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Made by Andrew Burks with React, Material-UI, and Font-Awesome.
+Made by Andrew Burks with React and Font-Awesome.
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ precache-manifest.574ca4b9fdddde85163533bdf0fab32e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00abd3feb7025deabc5b",
+    "url": "/static/js/main.00abd3fe.chunk.js"
+  },
+  {
+    "revision": "da675003d33245eaa63e",
+    "url": "/static/js/1.da675003.chunk.js"
+  },
+  {
+    "revision": "00abd3feb7025deabc5b",
+    "url": "/static/css/main.f81415f8.chunk.css"
+  },
+  {
+    "revision": "da675003d33245eaa63e",
+    "url": "/static/css/1.caba4129.chunk.css"
+  },
+  {
+    "revision": "be5b598b4b446202f60979f1865fff02",
+    "url": "/index.html"
+  }
+];
