1e8b5af5580b6ed66b355e7d7872bb0b99703130
Andrew Burks <andrewtburks@gmail.com>
2019-09-09 17:09:50 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Made by Andrew Burks with React and Font-Awesome.
-
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ styles-679fb12116ea64dcb5ba.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{209:function(n,o,c){},210:function(n,o,c){},211:function(n,o,c){},212:function(n,o,c){},256:function(n,o,c){},258:function(n,o,c){}}]);
+//# sourceMappingURL=styles-679fb12116ea64dcb5ba.js.map
